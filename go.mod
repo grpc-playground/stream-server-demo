0e6e1fd1ad35e3564f2d6dfe3d9c38dc5e4dddf3
solarhell <songjiaxin2008@gmail.com>
2020-03-31 23:12:58 +0800

    init

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/grpc-playground/stream-server-demo
+
+go 1.14
+
+require (
+	github.com/grpc-playground/proto v0.0.0-20200331150947-43f08613f1ea
+	google.golang.org/grpc v1.28.0
+)
---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	pb "github.com/grpc-playground/proto/pb"
+	"io"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type server struct{}
+
+func (s *server) SayHello(gs pb.Greeter_SayHelloServer) error {
+	for {
+		in, err := gs.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			log.Printf("failed to recv: %v", err)
+			return err
+		}
+
+		gs.Send(&pb.HelloReply{Message: "Hello " + in.Name})
+	}
+
+	return nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
